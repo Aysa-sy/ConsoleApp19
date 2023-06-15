c4d9e32bced04a459a5fcda89a0658ffc329b6b1
aysa-sy <aisa.soleimani83@gmail.com>
2023-06-15 22:39:31 +0330

    Add project files.

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter x:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("enter y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double result = Math.Log(x) / Math.Log(y);
+
+            Console.WriteLine($"log-{y}({x})={result}");
+        }
+    }
+}
